4132bfdac65c0baa982547d830a72e1860044003
letuu <albertvt@ya.ru>
2022-11-02 01:20:51 +0300

    Выполнил дз Enum

---
+++ src/pro/sky/java/ds_3_0/task1/Truck.java
@@ -0,0 +1,67 @@
+package pro.sky.java.ds_3_0.task1;
+
+public class Truck extends Transport implements Competing {
+
+    private LoadCapacity loadCapacity;
+
+    public Truck(String brand, String model, float engineVolume, LoadCapacity loadCapacity) {
+        super(brand, model, engineVolume);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void startMoving() {
+        System.out.printf("Грузовая машина %s %s поехала\n",
+                this.getBrand(),
+                this.getModel());
+    }
+
+    @Override
+    public void stopMoving() {
+        System.out.printf("Грузовая машина %s %s остановилась\n",
+                this.getBrand(),
+                this.getModel());
+    }
+
+    @Override
+    public void printType() {
+        if (loadCapacity != null) {
+            System.out.println("Тип грузоподъемности грузовика: " + loadCapacity);
+        } else {
+            System.out.println("Данных по грузовому авто недостаточно");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" +
+                "transport=" + super.toString() +
+                ", loadCapacity='" + loadCapacity + '\'' +
+                '}';
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.printf("Грузовая машина %s %s совершила Пит-стоп\n",
+                this.getBrand(),
+                this.getModel());
+    }
+
+    @Override
+    public int getBestLapTime() {
+        return 646;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return 205;
+    }
+
+    public LoadCapacity getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(LoadCapacity loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+}
